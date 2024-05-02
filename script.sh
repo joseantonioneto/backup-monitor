13138ea35a6634809d1cc7d8481205cbb5be0391
joseantonioneto <antonio.souzneto@gmail.com>
2024-05-02 09:37:54 -0300

    Adicioando informações de porcentagem de uso e data atual

--- script.sh
+++ script.sh
@@ -6,5 +6,11 @@ backup_size_gb=$(echo "scale=2; $backup_size / 1024^3" | bc)
 
 memory_size=$(df -h / | awk '/\// {print $4}')
 
+percent_use=$(df -h / | awk '/\// {print $5}')
+
+present_date=$(date +"%Y-%m-%d")
+
 echo "Espaço ocupado de Backup: $backup_size_gb GB"
 echo "Espaço disponível: $memory_size"
+echo "Uso de espaço de disco: $percent_use"
+echo "Data atual: $present_date"
